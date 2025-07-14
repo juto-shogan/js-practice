677f34322cbe0ba9753b84004d53aa6fc39f805a
juto-shogan <somtombonu53@gmail.com>
2025-07-14 19:55:58 +0100

    modifications

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,8 +3,20 @@ const router = express.Router();
 
 // Routes
 
-router.get('/', (req, res) => {
-  res.send('Hello World!');
+router.get('', (req, res) => {
+  const locals = {
+    title: 'NodeJS blog',
+    description: 'simple web made with express and ejs'
+  }
+  res.render('index', {locals});
+});
+
+router.get('/about', (req, res) => {
+  res.render('about');
+});
+
+router.get('/contact', (req,res) =>{
+  res.render('contact');
 });
 
 module.exports = router;
